33eafc77b9f8ae583f7d6f0b0fcd8b918afa9698
Yuriy <45271031+yurka1605@users.noreply.github.com>
2020-11-19 15:18:58 +0400

    Delete initialization app in function.js

--- functions.js
+++ functions.js
@@ -1,16 +1,3 @@
-// Initialize the application
-var storeData = EcwidApp.getPayload();
-var storeId = storeData.store_id;
-var accessToken = storeData.access_token;
-var language = storeData.lang;
-var viewMode = storeData.view_mode;
-if (storeData.public_token !== undefined){
-  var publicToken = storeData.public_token;
-}
-if (storeData.app_state !== undefined){
-  var appState = storeData.app_state;
-}
-
 function readValuesFromPage(){
 	var applicationConfig = {
 		public: {},
