309efa19fbad6b5db051e1516f97b218c791d1b6
Litvinov Yuriy <lua@hlcompany.ru>
2020-12-28 11:34:29 +0400

    modified config

--- functions.js
+++ functions.js
@@ -12,8 +12,6 @@ var accessToken = storeData.access_token;
 var language = storeData.lang;
 var viewMode = storeData.view_mode;
 
-console.log(storeData);
-
 if (storeData.public_token !== undefined){
   var publicToken = storeData.public_token;
 }
@@ -22,13 +20,22 @@ if (storeData.app_state !== undefined){
   var appState = storeData.app_state;
 }
 
+function showEditPage(elementClass){
+	document.querySelector('.main').style.display = 'none';
+	document.querySelector(elementClass).style.display = 'block';
+	document.querySelector('.content-control-menu-nav').style.display = 'flex';
+}
+
 function readValuesFromPage(){
+
 	var applicationConfig = {
 		public: {},
 		private: {}
 	}
+
 	var allInputs = document.querySelectorAll('input, select, textarea');
-	for (i = 0; i < allInputs.length; i++){
+
+	for (i=0; i<allInputs.length; i++){
 		var fieldVisibility = allInputs[i].dataset.visibility;
 
 		if(fieldVisibility !== undefined){
@@ -46,20 +53,23 @@ function readValuesFromPage(){
 			}
 		}
 	}
+
 	applicationConfig.public = JSON.stringify(applicationConfig.public);
+
 	return applicationConfig;
 }
 
 function setValuesForPage(applicationConfig){
+
 	var applicationConfigTemp = {
 		public: {},
 		private: {}
 	};
-	console.log(applicationConfig);
 
 	// for cases when we get existing users' data
+
 	if (applicationConfig.constructor === Array){
-		for (i = 0; i < applicationConfig.length; i++) {
+		for (i=0; i < applicationConfig.length; i++) {
 			if (applicationConfig[i].key !== 'public'){
 				applicationConfigTemp.private[applicationConfig[i].key] = applicationConfig[i].value;
 			} else {
@@ -73,6 +83,7 @@ function setValuesForPage(applicationConfig){
 	var allInputs = document.querySelectorAll('input, select, textarea');
 
 	// Set values from config for input, select, textarea elements
+	
 	for (i=0; i<allInputs.length; i++){
 		var fieldVisibility = allInputs[i].dataset.visibility;
 
@@ -98,8 +109,18 @@ function setValuesForPage(applicationConfig){
 
 // Default settings for new accounts
 var initialConfig = {
-	public: {},
-	private: {}
+	public: {
+		name: '',
+		organization_id: '',
+		login_iiko: '',
+		password_iiko: '',
+	},
+	private: {
+		id: '',
+		store_id: storeId,
+		token_app: accessToken,
+		installed: "true",
+	}
 };
 
 initialConfig.public = JSON.stringify(initialConfig.public);
@@ -118,15 +139,12 @@ function createUserData() {
 }
 
 
-// Executes if we have a user who logs in to the app not the first time. We load their preferences from Application storage with Ecwid JS SDK and display them in the app interface
 function getUserData() {
-	// Retrieve all keys and values from application storage, including public app config. Set the values for select, input and textarea elements on a page in a callback
 	EcwidApp.getAppStorage(function(allValues){
 		setValuesForPage(allValues);
 	});
 }
 
-// Executes when we need to save data. Gets all elements' values and saves them to Application storage and public app config via Ecwid JS SDK
 function saveUserData() {
 	var saveData = readValuesFromPage();
 	EcwidApp.setAppStorage(saveData.private, function(savedData){
@@ -137,8 +155,6 @@ function saveUserData() {
 	})
 }
 
-
-// Main app function to determine if the user is new or just logs into the app
 EcwidApp.getAppStorage('installed', function(value){
 	if (value != null) {
 		getUserData();
@@ -147,9 +163,7 @@ EcwidApp.getAppStorage('installed', function(value){
 	}
 })
 
-request();
-
-async function request() {
+async function saveClientData() {
 	const response = await fetch(apiUrl, {
 		method: 'POST',
 		mode: 'cors',
